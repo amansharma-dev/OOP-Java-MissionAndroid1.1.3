4c7a020389eaa883572c73ce07a3fc1956816c6d
amansharma-dev <amansharma.11@hotmail.com>
2020-03-11 17:38:33 +0530

    new java package commit oop

--- OOPClass/src/main/java/com/example/oopclass/Car.java
+++ OOPClass/src/main/java/com/example/oopclass/Car.java
@@ -1,8 +1,76 @@
 package com.example.oopclass;
 
 public class Car {
-    String companyName;
-    String carName;
-    int modal;
-    int price;
+    private String companyName;
+    private String carName;
+    private int modal;
+    private int price;
+
+    //constructor
+    public Car(){
+
+    }
+
+    public Car(String companyName, String carName, int modal, int price) {
+        this.companyName = companyName;
+        this.carName = carName;
+        this.modal = modal;
+        this.price = price;
+    }
+
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getModal() {
+        return modal;
+    }
+
+    public void setModal(int modal) {
+        this.modal = modal;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    //    public Car(String companyName,String carName, int modal,int price){
+//
+//    }
+
+
+    public void turnOff(){
+        System.out.println(this.carName+" Turn Off");
+    }
+
+    public void turnOn(){
+        System.out.println(this.carName+" Turn On");
+    }
+
+    public void gearMode(){
+        System.out.println(this.carName+" Neutral");
+    }
+
+    public String carDescription(){
+        return "Company Name is " +this.companyName + " & the car name is " +this.carName+
+                " of modal " +this.modal+ ", with the price of "+this.price;
+    }
 }
--- OOPClass/src/main/java/com/example/oopclass/MyClass.java
+++ OOPClass/src/main/java/com/example/oopclass/MyClass.java
@@ -3,20 +3,33 @@ package com.example.oopclass;
 public class MyClass {
 
     public static void main(String[] args) {
-        Car car = new Car();
-        car.companyName="BMW";
-        car.carName="z4";
-        car.modal=2019;
-        car.price=7500000;
-        System.out.println(car.companyName+" "+car.carName+" "+car.modal+" "+car.price);
+        Car car = new Car("BMW","z4",2019,7500000);
+//        car.companyName="BMW";
+//        car.carName="z4";
+//        car.modal=2019;
+//        car.price=7500000;
+        car.setPrice(7900000);
+        System.out.println("New Car Price: "+ car.getPrice());
 
 
-        Car car1 = new Car();
-        car.companyName="Mercedes";
-        car.carName="e63";
-        car.modal=2020;
-        car.price=6500000;
-        System.out.println(car.companyName+" "+car.carName+" "+car.modal+" "+car.price);
+        Car car1 = new Car("Mercedes","e63",2020,6500000);
+//        car1.companyName="Mercedes";
+//        car1.carName="e63";
+//        car1.modal=2020;
+//        car1.price=6500000;
 
+        car.turnOn();
+        car.turnOff();
+        car.gearMode();
+
+        car1.turnOn();
+        car1.turnOff();
+        car1.gearMode();
+
+        System.out.println(car.carDescription());
+        System.out.println(car1.carDescription());
+
+        Car car2 = new Car("Mini","MiniCooper",2021,4300000);
+        System.out.println(car2.carDescription());
     }
 }
